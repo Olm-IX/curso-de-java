55f4c230a9a34b2cc773296afd39c73aaec5a3b5
Naila <nailaoli@gmail.com>
2020-03-31 19:48:06 -0300

    Observação sobre default methods

--- 023interfaces/src/model/services/TaxService.java
+++ 023interfaces/src/model/services/TaxService.java
@@ -2,8 +2,24 @@ package model.services;
 
 public interface TaxService {
 
-	//"public abstract double tax (double amount)"; -> os métodos em interfaces 
-	//já são por padrão publicos e abstratos, por isso nem se costuma
-	//escrever isso no código
-	double tax (double amount);
+	// "public abstract double tax (double amount)"; -> os métodos em interfaces
+	// já são por padrão publicos e abstratos, por isso nem se costuma
+	// escrever isso no código
+	double tax(double amount);
+
+	// OBS.: a partir do java 8, as interfaces podem implementar métodos usando a
+	// palavra chave "default". Esse método poderá ser reutilizado por classes que implementam o
+	// contrato, ou podem ser sobrescritos. UMA VANTAGEM, REUTILIZAÇÃO DE CÓDIGO! Que antes só
+	// era possível através da herança.
+	// Exemplo de como ficaria um método default para calcular o imposto:
+	//
+	// double getTaxPercentage();
+	//
+	// default double tax(double amount) {
+	//		return amount*getTaxPercentage();
+	//	}
+	//
+	// Seria possível, nas classes que assinam o contrato, criar um atributo com o valor do 
+	// percentual de imposto de acordo com o país, e seu respectivo getter (com o @Override), e daí 
+	// poderia utilizar esse método padrão da interface.
 }
